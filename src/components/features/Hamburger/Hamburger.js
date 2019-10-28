5b48356c75b12ecceada1b1080bc8ab3366cfb11
Piotr Nowinski <pitred007git@gmail.com>
2019-10-28 20:10:12 +0100

    Fix bugs

--- src/components/features/Hamburger/Hamburger.js
+++ src/components/features/Hamburger/Hamburger.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Hamburger.scss';
 
 const Hamburger = ({ toggleMenu, menuShow }) => {
-   const hamburgerClassName = menuShow ? 'hamburger--close' : '';
+   const hamburgerClassName = menuShow ? ' hamburger--close' : '';
    return (
       <div className={`hamburger${hamburgerClassName}`} onClick={toggleMenu}>
          <span className='hamburger__line'></span>
